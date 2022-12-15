0d16784b3d8ead10d4a6119a6d7bcdb0577978ab
Scott Zuvich <scott.zuvich@student.csulb.edu>
2022-12-14 22:37:22 -0800

    completion

--- dblib.js
+++ dblib.js
@@ -24,11 +24,7 @@ const getTotalRecords = () => {
             }
         });
 };
-const importCustomers = ( customers ) => {
-    customers.forEach( customer => {
-        insertCustomer( customer.split( ',' ) );
-    });
-}
+
 const insertCustomer = (cust) => {
     if (cust instanceof Array) {
         params = cust;
@@ -53,22 +49,71 @@ const insertCustomer = (cust) => {
             };
         });
 };
+const updateCustomer = (cust) => {
+    if (cust instanceof Array) {
+        params = cust;
+    } else {
+        params = Object.values(cust);
+    };
+
+    const sql = `UPDATE customer SET cusFname = $2, cusLname = $3,  cusState = $4, cusSalesYTD = $5, cusSalesPrev = $6 WHERE cusId = $1`;
+
+    return pool.query(sql, params)
+        .then(res => {
+            console.log( res );
+            return {
+                status: "success", 
+                msg: `Customer id ${params[0]} successfully updated`
+            };
+        })
+        .catch(err => {
+            console.log( err );
+            return {
+                status: "fail", 
+                msg: `Error on update of customer id ${params[0]}.  ${err.message}`
+            };
+        });
+};
+
+
+const deleteCustomer = ( cusId ) => {
+    //console.log( cusId );
+    params = [cusId];
+    const sql = `DELETE FROM customer WHERE cusId IN ($1)`;
+console.log( params );
+    return pool.query(sql, params)
+        .then(res => {
+            return {
+                trans: "success", 
+                msg: `Customer id ${params[0]} successfully deleted`
+            };
+        })
+        .catch(err => {
+            return {
+                trans: "fail", 
+                msg: `Error on insert of customer id ${params[0]}.  ${err.message}`
+            };
+        });
+};
+
 const getCustomer = ( cusId ) => {
     //console.log( cusId );
     params = [cusId];
     sql = "SELECT * FROM customer WHERE cusId = $1";
     return pool.query(sql, params)
-        .then(result => {
+        .then( result => {
             //console.log( result );
             return { 
-                trans: "success",
-                result: result.rows
+                status: "success",
+                result: result.rows,
+                message: ''
             }
         })
         .catch(err => {
             return {
-                trans: "Error",
-                result: `Error: ${err.message}`
+                status: "Error",
+                result: '',
+                message: `Error: ${err.message}`
             }
         });
 
@@ -78,37 +123,38 @@ const findCustomers = (cust) => {
     var i = 1;
     params = [];
     sql = "SELECT * FROM customer WHERE true";
-    
-    if (cust.cusId !== "") {
-        params.push(Number(cust.cusId));
-        sql += ` AND cusId = $${i}`;
-        i++;
-    };
-    if (cust.cusFname !== "") {
-        params.push(`${cust.cusFname}%`);
-        sql += ` AND UPPER(cusFname) LIKE UPPER($${i})`;
-        i++;
-    };
-    if (cust.cusLname !== "") {
-        params.push(`${cust.cusLname}%`);
-        sql += ` AND UPPER(cusLname) LIKE UPPER($${i})`;
-        i++;
-    };
-    if (cust.cusState !== "") {
-        params.push(`${cust.cusState}`);
-        sql += ` AND cusState = $${i}`;
-        i++;
-    };
-    if (cust.cusSalesYTD !== "") {
-        params.push(parseFloat(cust.cusSalesYTD));
-        sql += ` AND cusSalesYTD >= $${i}`;
-        i++;
-    };
-    if (cust.cusSalesPrev !== "") {
-        params.push(parseFloat(cust.cusSalesPrev));
-        sql += ` AND cusSalesPrev >= $${i}`;
-        i++;
-    };
+    if( typeof( cust ) !== 'undefined' ){
+        if (cust.cusId !== "") {
+            params.push(Number(cust.cusId));
+            sql += ` AND cusId = $${i}`;
+            i++;
+        };
+        if (cust.cusFname !== "") {
+            params.push(`${cust.cusFname}%`);
+            sql += ` AND UPPER(cusFname) LIKE UPPER($${i})`;
+            i++;
+        };
+        if (cust.cusLname !== "") {
+            params.push(`${cust.cusLname}%`);
+            sql += ` AND UPPER(cusLname) LIKE UPPER($${i})`;
+            i++;
+        };
+        if (cust.cusState !== "") {
+            params.push(`${cust.cusState}`);
+            sql += ` AND UPPER(cusState) = UPPER($${i})`;
+            i++;
+        };
+        if (cust.cusSalesYTD !== "") {
+            params.push(parseFloat(cust.cusSalesYTD));
+            sql += ` AND cusSalesYTD >= $${i}`;
+            i++;
+        };
+        if (cust.cusSalesPrev !== "") {
+            params.push(parseFloat(cust.cusSalesPrev));
+            sql += ` AND cusSalesPrev >= $${i}`;
+            i++;
+        };
+    }
 
     sql += ` ORDER BY cusId`;
     // for debugging
@@ -132,7 +178,8 @@ const findCustomers = (cust) => {
 
 // Add towards the bottom of the page
 module.exports.getCustomer = getCustomer;
+module.exports.updateCustomer = updateCustomer;
 module.exports.findCustomers = findCustomers;
+module.exports.deleteCustomer = deleteCustomer;
 module.exports.insertCustomer = insertCustomer;
-module.exports.importCustomers = importCustomers;
 module.exports.getTotalRecords = getTotalRecords;
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // Required modules 
 const express = require("express");
 const dblib = require("./dblib.js");
-
+const fs = require( 'fs' );
 const multer = require("multer");
 
 const app = express();
-
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
@@ -14,6 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // Setup EJS
 app.set("view engine", "ejs");
+
 // Enable CORS (see https://enable-cors.org/server_expressjs.html)
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -23,77 +23,19 @@ app.use((req, res, next) => {
     );
     next();
 });
+
 // Application folders
 app.use(express.static("public"));
 
 // Setup routes
+
 app.get("/", (req, res) => {
-    //res.send("Root resource - Up and running!")
     res.render("index");
 });
 
-app.get("/edit/:cusId", (req, res) => {
-    dblib.getCustomer( req.params.cusId )
-        .then( result => {
-            res.render("edit", {
-                cusId: req.params.cusId,
-                result: result.result
-            });
-        })
-        .catch( err => {
-            res.render("edit", {
-                cusId: req.params.cusId,
-                cust: `Unexpected Error: ${err.message}`
-            });
-        });
-});
-
-app.get( "/import", async ( req, res ) => {
-    const totRecs = await dblib.getTotalRecords();
-    res.render( "import", {totRecs: totRecs.totRecords } );
-});
-app.post("/import", upload.single("filename"), async ( req, res, next ) => { // myFile should be the same value as used in HTML name attribute of input
-    const file = req.file; // We get the file in req.file
-  
-    if (!file) { // in case we do not get a file we return
-      const error = new Error("Please upload a file");
-      error.httpStatusCode = 400;
-      return req.next(error);
-    }
-    const multerText = Buffer.from(file.buffer).toString("utf-8"); // this reads and converts the contents of the text file into string
-
-    const customers = multerText.split( "\r\n" );
-    customers.forEach( customer => {
-        dblib.insertCustomer( customer.split( ',' ) );
-    });
-    const totRecs = await dblib.getTotalRecords();
-    res.render( "import", { totRecs: totRecs.totRecords } );
-});
-
-app.get( "/create", ( req, res ) => {
-    res.render( "create" );
-});
-
-app.post( "/create", (req, res ) => {
-    dblib.insertCustomer( req.body )
-        .then(result => {
-            res.render("create", {
-                type: "post",
-                message: "Customer Created Successfully!"
-            })
-        })
-        .catch(err => {
-            res.render("create", {
-                type: "post",
-                message: "Customer Creation Failed!"
-            })
-        });
-});
 
 app.get("/manage", async (req, res) => {
-    // Omitted validation check
     const totRecs = await dblib.getTotalRecords();
-    //Create an empty product object (To populate form with values)
     const cust = {
         cusId: "",
         cusFname: "",
@@ -108,13 +50,10 @@ app.get("/manage", async (req, res) => {
         cust: cust
     });
 });
-app.post("/manage", async (req, res) => {
-    // Omitted validation check
-    //  Can get totRecs from the page rather than using another DB call.
-    //  Add it as a hidden form value.
-    const totRecs = await dblib.getTotalRecords();
 
-    dblib.findCustomers(req.body)
+app.post("/manage", async (req, res) => {
+    const totRecs = await dblib.getTotalRecords();
+    dblib.findCustomers( req.body )
         .then(result => {
             res.render("manage", {
                 type: "post",
@@ -133,19 +72,238 @@ app.post("/manage", async (req, res) => {
         });
 });
 
-app.get("/searchajax", async (req, res) => {
-    // Omitted validation check
-    const totRecs = await dblib.getTotalRecords();
-    res.render("searchajax", {
-        totRecs: totRecs.totRecords,
+
+app.get( "/create", ( req, res ) => {
+    res.render( "create" , {
+        type: "get",
+        cusid: '',
+        cusfname: '',
+        cuslname: '',
+        cusstate: '',
+        cussalesytd: '',
+        cussalesprev: '',
+        message: ""
     });
 });
-app.post("/searchajax", upload.array(), async (req, res) => {
-    dblib.findProducts(req.body)
-        .then(result => res.send(result))
-        .catch(err => res.send({trans: "Error", result: err.message}));
 
+app.post( "/create", (req, res ) => {
+    dblib.insertCustomer( req.body )
+        .then(result => {
+            res.render("create", {
+                type: "post",
+                cusid: req.body.cusId,
+                cusfname: req.body.cusFname,
+                cuslname: req.body.cusLname,
+                cusstate: req.body.cusState,
+                cussalesytd: req.body.cusSalesYTD,
+                cussalesprev: req.body.cusSalesPrev,
+                message: "Customer Created Successfully!"
+            })
+        })
+        .catch(err => {
+            res.render("create", {
+                type: "post",
+                cusid: req.body.cusId,
+                cusfname: req.body.cusFname,
+                cuslname: req.body.cusLname,
+                cusstate: req.body.cusState,
+                cussalesytd: req.body.cusSalesYTD,
+                cussalesprev: req.body.cusSalesPrev,
+                message: "Customer Creation Failed!"
+            })
+        });
 });
+
+app.get("/edit/:cusId", (req, res) => {
+    dblib.getCustomer( req.params.cusId )
+        .then( result => {
+            console.log( result );
+            res.render("edit", {
+                type: 'get',
+                cusid: result.result[0].cusid,
+                cusfname: result.result[0].cusfname,
+                cuslname: result.result[0].cuslname,
+                cusstate: result.result[0].cusstate,
+                cussalesytd: result.result[0].cussalesytd,
+                cussalesprev: result.result[0].cussalesprev,
+                message: ''
+            });
+        })
+        .catch( err => {
+            res.render("edit", {
+                type: 'get',
+                cusid: '',
+                cusfname: '',
+                cuslname: '',
+                cusstate: '',
+                cussalesytd: '',
+                cussalesprev: '',
+                message: `Unexpected Error: ${err.message}`
+            });
+        });
+});
+app.post("/edit/:cusId", (req, res) => {
+    console.log( req.body );
+    dblib.updateCustomer( req.body );
+    dblib.getCustomer( req.params.cusId )
+        .then( result => {
+            res.render("edit", {
+                type: 'post',
+                cusid: req.body.cusId,
+                cusfname: req.body.cusFname,
+                cuslname: req.body.cusLname,
+                cusstate: req.body.cusState,
+                cussalesytd: req.body.cusSalesYTD,
+                cussalesprev: req.body.cusSalesPrev,
+                message: 'Customer updated Successfully'
+            });
+        })
+        .catch( err => {
+            res.render("edit", {
+                type: 'post',
+                cusid: '',
+                cusfname: '',
+                cuslname: '',
+                cusstate: '',
+                cussalesytd: '',
+                cussalesprev: '',
+                message: `Unexpected Error: ${err.message}`
+            });
+        });
+});
+
+app.get("/delete/:cusId", (req, res) => {
+    dblib.getCustomer( req.params.cusId )
+        .then( result => {
+            res.render("delete", {
+                type: 'get',
+                cusid: result.result[0].cusid,
+                cusfname: result.result[0].cusfname,
+                cuslname: result.result[0].cuslname,
+                cusstate: result.result[0].cusstate,
+                cussalesytd: result.result[0].cussalesytd,
+                cussalesprev: result.result[0].cussalesprev,
+                message: ''
+            });
+        })
+        .catch( err => {
+            res.render("delete", {
+                type: 'get',
+                cusid: '',
+                cusfname: '',
+                cuslname: '',
+                cusstate: '',
+                cussalesytd: '',
+                cussalesprev: '',
+                message: `Unexpected Error: ${err.message}`
+            });
+        });
+});
+
+app.post("/delete/:cusId", (req, res) => {
+    dblib.deleteCustomer( req.body.cusId )
+        .then( result => {
+            res.render("delete", {
+                type: 'post',
+                cusid: req.body.cusId,
+                cusfname: req.body.cusFname,
+                cuslname: req.body.cusLname,
+                cusstate: req.body.cusState,
+                cussalesytd: req.body.cusSalesYTD,
+                cussalesprev: req.body.cusSalesPrev,
+                message: 'Customer Deleted Successfully'
+            });
+        })
+        .catch( err => {
+            res.render("delete", {
+                type: 'post',
+                cusid: '',
+                cusfname: '',
+                cuslname: '',
+                cusstate: '',
+                cussalesytd: '',
+                cussalesprev: '',
+                message: `Unexpected Error: ${err.message}`
+            });
+        });
+});
+
+app.get( "/import", async ( req, res ) => {
+    const totRecs = await dblib.getTotalRecords();
+    res.render( "import", {
+        type: 'get',
+        totRecs: totRecs.totRecords
+    });
+});
+
+app.post("/import", upload.single("filename"), async ( req, res, next ) => {
+    const file = req.file; 
+    let recsProcessed = 0;
+    let recsSuccessful = 0;
+    let recsFailed = 0;
+    let errors = [];
+    if (!file) { 
+      const error = new Error("Please upload a file");
+      error.httpStatusCode = 400;
+      return req.next(error);
+    }
+    const multerText = Buffer.from(file.buffer).toString("utf-8"); 
+
+    const customers = multerText.split( "\r\n" );
+    for( customer of customers ) {
+        recsProcessed++;
+        await dblib.insertCustomer( customer.split( ',' ) )
+            .then( result =>{
+                if( result.trans == 'success' ) {
+                    recsSuccessful++;
+                } else if( result.trans == 'fail' ) {
+                    recsFailed++;
+                    errors.push( result.msg );
+                }
+            }).catch( err => {
+                console.log( err.message );
+            });
+    };
+    const totRecs = await dblib.getTotalRecords();
+    res.render( "import", {
+        type: 'post',
+        totRecs: totRecs.totRecords,
+        recsProcessed: recsProcessed,
+        recsSuccessful: recsSuccessful,
+        recsFailed: recsFailed,
+        errors: errors
+    });
+});
+
+app.get( "/export", async ( req, res ) => {
+    const totRecs = await dblib.getTotalRecords();
+    res.render( "export", {totRecs: totRecs.totRecords } );
+});
+
+
+app.post( "/export", (req, res ) => {
+    let filename = req.body.filename;
+    let textContent = '';
+    dblib.findCustomers()
+        .then(result => {
+            //console.log( result );
+            let i = 1;
+            for( customer of result.result ) {
+                if( i > 1 ) { prefix = '\r\n'; } else { prefix = ''; }
+                textContent += `${prefix}${customer.cusid},${customer.cusfname},${customer.cuslname},${customer.cusstate},${customer.cussalesytd.replace(/\$/g, '').replace(/,/g, '')},${customer.cussalesprev.replace(/\$/g, '').replace(/,/g, '')}`
+                i++;
+            }
+            res.setHeader('Content-type', "application/octet-stream");
+            res.setHeader('Content-disposition', `attachment; filename=${filename}`);
+            res.send( textContent );
+        })
+        .catch(err => {
+            res.send( err.message );
+        });
+});
+
+
+
 // Start listener
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server started (http://localhost:3000/) !");
